e2edd5ec8c6d85acfb2ae55e3e759bd5957e0983
Gabriel Alejandro Cruz Soto <gabrielcrsoto@outlook.com>
2021-11-03 22:37:09 -0300

    Proyect initial

---
+++ src/main/java/cl/gabrielcrsoto/srt/backend/SrtBkApplication.java
@@ -0,0 +1,13 @@
+package cl.gabrielcrsoto.srt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrtBkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/gabrielcrsoto/srt/backend/SrtBkApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gabrielcrsoto.srt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
